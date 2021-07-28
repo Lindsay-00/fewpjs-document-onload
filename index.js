6970f46038dd0613d45e59a856db71aa5d71081a
Lindsay <lindsay.chen@yale.edu>
2021-07-29 07:34:27 +0800

    commit tree 0479a86fb0ee572f614fcb5e5436f1d20baf701b

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    // const text = document.getElementById("text")
+    document.querySelector('p#text').textContent = "This is really cool!"
+  });
+
